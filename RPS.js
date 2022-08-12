668196cdfdfdce5a5c5d880029dbedbb6350c1a5
mremoto <mremotoo@gmail.com>
2022-08-12 11:53:17 +0800

    Modify js file to get random computer answer.

--- RPS.js
+++ RPS.js
@@ -1,2 +1,8 @@
 console.log("Hello World!")
 
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    console.log( choices[Math.floor(Math.random() * choices.length)])
+}
+
+getComputerChoice();
