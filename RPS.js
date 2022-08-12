b54af682dfe19ccdc69a6e7b4a36fb0ccac7feac
mremoto <mremotoo@gmail.com>
2022-08-12 12:29:17 +0800

    Add a playRound function.

--- RPS.js
+++ RPS.js
@@ -1,8 +1,20 @@
-console.log("Hello World!")
-
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
-    console.log( choices[Math.floor(Math.random() * choices.length)])
+    return( choices[Math.floor(Math.random() * choices.length)])
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === playerSelection && computerSelection === "Paper") {
+        return("Computer chose Paper. You Lose!")
+    }else if (playerSelection === playerSelection && computerSelection === "Scissors") {
+        return("Computer chose Scissors. You Win!")
+    }
+    
+    else{
+        return("Computer chose Rock. Draw!")
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
