2a33bd6c02c5a921f84f9ad7ebeb492b96c98abd
mremoto <mremotoo@gmail.com>
2022-08-14 11:31:12 +0800

    Modify playround conditional logic.

--- RPS.js
+++ RPS.js
@@ -1,29 +1,58 @@
 function getPlayerChoice() {
     //get player input and make everything toLoweCase
     let choice = prompt("Enter your choice.").toLowerCase();
+    return(choice);
 }
 
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
-    return( choices[Math.floor(Math.random() * choices.length)])
+    return( choices[Math.floor(Math.random() * choices.length)]);
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection,) {
     if (playerSelection != undefined) {
-        
+        if (playerSelection === "rock"){
+            if (computerSelection === "rock"){
+                return("Computer chose rock. Draw!")
+            }
+            if (computerSelection === "paper"){
+                return("Computer chose paper. You Lose!")
+            }
+            if (computerSelection === "scissors"){
+                return("Computer chose scissors. You Win!")
+            }
+        }
+        if (playerSelection === "paper"){
+            if (computerSelection === "rock"){
+                return("Computer chose rock. You Win!")
+            }
+            if (computerSelection === "paper"){
+                return("Computer chose paper. Draw!")
+            }
+            if (computerSelection === "scissors"){
+                return("Computer chose scissors. You Lose!")
+            }
+        }
+        if (playerSelection === "scissors"){
+            if (computerSelection === "rock"){
+                return("Computer chose rock. You Lose!")
+            }
+            if (computerSelection === "paper"){
+                return("Computer chose paper. You Win!")
+            }
+            if (computerSelection === "scissors"){
+                return("Computer chose scissors. Draw!")
+            }
+        }
     } else{
         return("No choice entered.");
-        game();
     }
 }
 
 function game() {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
-    const gameRound = playRound(playerSelection, computerSelection);
     console.log(playRound(playerSelection, computerSelection));
 }
-
-
 
 game();
